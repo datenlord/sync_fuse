ab20dc0c9d816f8c80119638a2448db3d97603a5
qi.tian <qit@isrc.iscas.ac.cn>
2020-08-27 00:03:37 -0700

    Use macro_rules to implement the Unit trait

--- src/fuse/utils.rs
+++ src/fuse/utils.rs
@@ -1,76 +1,76 @@
-/// A type cast trait used to do the integer arithmetic.
-pub trait Uint {
-    /// Overflow add.
-    fn overflow_add(self, other: Self) -> Self
-    where
-        Self: std::fmt::Display + num_traits::ops::checked::CheckedAdd,
-    {
-        let result = Self::checked_add(&self, &other);
-        if let Some(value) = result {
-            value
-        } else {
-            debug_assert!(
-                false,
-                "number = {} add number = {} overflowing",
-                self, other
-            );
-            self
-        }
-    }
+macro_rules! impl_from {
+    ($target: ty) => {
+        impl Uint<$target> for $target {
+            #[inline]
+            fn overflow_add(self, other: $target) -> Self {
+                let (res, overflow) = self.overflowing_add(other);
+                debug_assert!(
+                    !overflow,
+                    "number = {} add number = {} overflowing",
+                    self, other
+                );
+                res
+            }
 
-    /// Overflow sub.
-    fn overflow_sub(self, other: Self) -> Self
-    where
-        Self: std::fmt::Display + num_traits::ops::checked::CheckedSub,
-    {
-        let result = Self::checked_sub(&self, &other);
-        if let Some(value) = result {
-            value
-        } else {
-            debug_assert!(
-                false,
-                "number = {} substract number = {} overflowing",
-                self, other
-            );
-            self
-        }
-    }
+            #[inline]
+            fn overflow_sub(self, other: $target) -> Self {
+                let (res, overflow) = self.overflowing_sub(other);
+                debug_assert!(
+                    !overflow,
+                    "number = {} substract number = {} overflowing",
+                    self, other
+                );
+                res
+            }
 
-    /// Overflow mul.
-    fn overflow_mul(self, other: Self) -> Self
-    where
-        Self: std::fmt::Display + num_traits::ops::checked::CheckedMul,
-    {
-        let result = Self::checked_mul(&self, &other);
-        if let Some(value) = result {
-            value
-        } else {
-            debug_assert!(
-                false,
-                "number = {} multiply number = {} overflowing",
-                self, other
-            );
-            self
-        }
-    }
+            fn overflow_mul(self, other: $target) -> Self {
+                let (res, overflow) = self.overflowing_mul(other);
+                debug_assert!(
+                    !overflow,
+                    "number = {} multiply number = {} overflowing",
+                    self, other
+                );
+                res
+            }
 
-    /// Overflow div.
-    fn overflow_div(self, other: Self) -> Self
-    where
-        Self: std::fmt::Display + num_traits::ops::checked::CheckedDiv,
-    {
-        let result = Self::checked_div(&self, &other);
-        if let Some(value) = result {
-            value
-        } else {
-            debug_assert!(
-                false,
-                "number = {} divide number = {} overflowing",
-                self, other
-            );
-            self
+            fn overflow_div(self, other: $target) -> Self {
+                let (res, overflow) = self.overflowing_div(other);
+                debug_assert!(
+                    !overflow,
+                    "number = {} divide number = {} overflowing",
+                    self, other
+                );
+                res
+            }
         }
     }
 }
+impl_from!(u8);
+impl_from!(u16);
+impl_from!(u32);
+impl_from!(u64);
+impl_from!(u128);
+impl_from!(i8);
+impl_from!(i16);
+impl_from!(i32);
+impl_from!(i64);
+impl_from!(i128);
+impl_from!(usize);
+impl_from!(isize);
 
-impl<T> Uint for T {}
+
+/// A type cast trait used to do the integer arithmetic.
+pub trait Uint<T> {
+    /// Overflow add.
+    fn overflow_add(self, other: Self) -> Self;
+
+    /// Overflow sub.
+    fn overflow_sub(self, other: Self) -> Self;
+
+    /// Overflow mul.
+    fn overflow_mul(self, other: Self) -> Self;
+
+    /// Overflow div.
+    fn overflow_div(self, other: Self) -> Self;
+}
+
